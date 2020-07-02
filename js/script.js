e492e2d8fdc6bcb94142e737bac05ba173f6a5ae
yun1025 <brown1025@naver.com>
2020-07-02 14:33:17 +0900

    Feat : 플로팅 메뉴, 메뉴 클릭시 스크롤 이동 기능 추가

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(function() {
+	var menu = $("header").offset();
 	
-})
+	$(window).scroll(function() {
+		if($(document).scrollTop() > menu.top) {
+			$("header").addClass("float");
+		}else {
+			$("header").removeClass("float");
+		}
+	});
+});
+
+//메뉴 스크롤 이동
+function menu_scroll(name) {
+	var offset = $("#" + name).offset();
+	
+	$("html").animate({scrollTop: offset.top}, 400);
+}
