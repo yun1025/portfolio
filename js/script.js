653b47b7df98ccbd3707d1647329e02f18fd7f49
yun1025 <brown1025@naver.com>
2020-07-02 15:15:21 +0900

    Feat : skill - 막대 그래프 애니메이션 구현

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function() {
 	//메뉴 상단 고정
 	var menu = $("header").offset();
+	var skill = $("#skill").offset();
 	
 	$(window).scroll(function() {
 		if($(document).scrollTop() > menu.top) {
@@ -8,9 +9,22 @@ $(function() {
 		}else {
 			$("header").removeClass("float");
 		}
-	});
+		
+		//skill 애니메이션
+		if($(document).scrollTop() > (skill.top - 700)) {
+			$("#skill ul li:nth-child(1) .stick").animate({width : "75%"}, 1000)
+			$("#skill ul li:nth-child(2) .stick").animate({width : "65%"}, 1200)
+			$("#skill ul li:nth-child(3) .stick").animate({width : "70%"}, 1400)
+			$("#skill ul li:nth-child(4) .stick").animate({width : "60%"}, 1600)
+			$("#skill ul li:nth-child(5) .stick").animate({width : "75%"}, 1800)
+			$("#skill ul li:nth-child(6) .stick").animate({width : "70%"}, 2000)
+		}
+	});	
 	
+	
+	//반응형
 	$(window).resize(function() {
+		//메뉴 슬라이드
 		if($(document).width() > 751) {		//모바일 사이즈가 아닐 때
 			$(".pc_menu").removeClass("on").show();
 		}else (
