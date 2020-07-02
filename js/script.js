b19350569086e7121bad8931c71190350592c63d
yun1025 <brown1025@naver.com>
2020-07-02 14:56:12 +0900

    Feat : 모바일 사이즈일 때 메뉴 슬라이드,  스크롤 이동 시 모바일 메뉴 슬라이드 기능 추가

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+	//메뉴 상단 고정
 	var menu = $("header").offset();
 	
 	$(window).scroll(function() {
@@ -8,11 +9,34 @@ $(function() {
 			$("header").removeClass("float");
 		}
 	});
+	
+	$(window).resize(function() {
+		if($(document).width() > 751) {		//모바일 사이즈가 아닐 때
+			$(".pc_menu").removeClass("on").show();
+		}else (
+			$(".pc_menu").removeClass("on").hide()
+		)
+	});
 });
 
 //메뉴 스크롤 이동
 function menu_scroll(name) {
+	slide_menu();
+	
 	var offset = $("#" + name).offset();
 	
 	$("html").animate({scrollTop: offset.top}, 400);
+}
+
+// 모바일 - 메뉴 슬라이드
+function slide_menu() {
+	if($(document).width() <= 751) {
+		var menu_class = $(".pc_menu").hasClass("on");
+		
+		if(menu_class == false) {		//모바일 사이즈일 때
+			$(".pc_menu").addClass("on").slideDown();
+		}else {
+			$(".pc_menu").removeClass("on").slideUp();
+		}
+	}
 }
